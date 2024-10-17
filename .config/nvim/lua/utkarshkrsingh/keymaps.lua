61d8710131cdd0ee6058d147b99a2d8c087fc294
Utkarsh Kr Singh <utkarshkrsingh@archlinux.localdomain>
2024-10-17 09:54:01 +0530

    nvim keymaps --updated

--- .config/nvim/lua/utkarshkrsingh/keymaps.lua
+++ .config/nvim/lua/utkarshkrsingh/keymaps.lua
@@ -3,11 +3,14 @@ local function map(mode, lhs, rhs)
 end
 
 -- Split
-map("n", "<leader>s", "<CMD>split<CR>")
-map("n", "<leader>vs", "<CMD>vsplit<CR>")
+map("n", "<leader>s", "<CMD>split<CR>")     -- horizontal split
+map("n", "<leader>vs", "<CMD>vsplit<CR>")   -- vertical split
 
 -- Terminal
 map("n", "<leader>t", "<CMD>terminal<CR>")
+
+-- Exit terminal
+map("t", "<Esc>", "<C-\\><C-n>")
 
 -- Save
 map("n", "<leader>w", "<CMD>update<CR>")
@@ -19,10 +22,10 @@ map("n", "<leader>q", "<CMD>q<CR>")
 map("n", "<leader>wq", "<CMD>wq<CR>")
 
 -- Window Navigation
-map("n", "<C-h>", "<C-w>h")
-map("n", "<C-l>", "<C-w>l")
-map("n", "<C-k>", "<C-w>k")
-map("n", "<C-j>", "<C-w>j")
+map("n", "<C-h>", "<C-w>h") -- move left
+map("n", "<C-l>", "<C-w>l") -- move right
+map("n", "<C-k>", "<C-w>k") -- move above
+map("n", "<C-j>", "<C-w>j") -- move belowe
 
 -- Resize Windows
 map("n", "<C-Left>", "<C-w><")
@@ -32,9 +35,3 @@ map("n", "<C-Down>", "<C-w>-")
 
 -- Lazy.nvim
 map("n", "<space><space>", "<CMD>Lazy<CR>")
-
--- Copy current word to system clipboard
-vim.api.nvim_set_keymap('n', '<leader>yw', 'viw"+y', { noremap = true, silent = true })
-
--- Copy current line to system clipboard
-vim.api.nvim_set_keymap('n', '<leader>yy', '"+yy', { noremap = true, silent = true })
