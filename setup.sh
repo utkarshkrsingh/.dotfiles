08326c2fbe56a3a51d313a162afa6d19ca29c7b8
utkarshkrsingh <utkarshkrsingh@proton.me>
2024-11-24 02:35:07 +0530

    config --updated

--- setup.sh
+++ setup.sh
@@ -1,42 +1,5 @@
 #!/bin/bash
 
-##################################################################################################################################################################################
-##################################################################################################################################################################################
-#  ______          _       
-# |  ____|        | |      
-# | |__ ___  _ __ | |_ ___ 
-# |  __/ _ \| '_ \| __/ __|
-# | | | (_) | | | | |_\__ \
-# |_|  \___/|_| |_|\__|___/
-#                          
-
-# Define the fonts directory
-FONT_DIR="$HOME/.local/share/fonts"
-
-# Check if the directory exists, and create it if not
-if [ ! -d "$FONT_DIR" ]; then
-    mkdir -p "$FONT_DIR"
-    echo "Created directory: $FONT_DIR"
-else
-    echo "Directory already exists: $FONT_DIR"
-fi
-
-# Download the font zip file
-curl -L -o "$FONT_DIR/FantasqueSansMono.zip" "https://github.com/ryanoasis/nerd-fonts/releases/download/v3.2.1/FantasqueSansMono.zip"
-
-# Japanese Language Support
-curl -L -o "notosanscjk-jp.ttf" "https://github.com/googlefonts/noto-cjk/raw/main/Sans/Variable/TTF/NotoSansCJKjp-VF.ttf"
-
-# move the jp font to font_dir
-mv "notosanscjk-jp.ttf" "$FONT_DIR/notosanscjk-jp.ttf"
-
-# Unzip the downloaded font and refresh the font cache
-unzip "$FONT_DIR/FantasqueSansMono.zip" -d "$FONT_DIR"
-fc-cache -fv
-echo "Font installed successfully."
-
-# delete the zip file
-rm -rf "$FONT_DIR/FantasqueSansMono.zip"
 
 ##################################################################################################################################################################################
 ##################################################################################################################################################################################
@@ -112,6 +75,44 @@ stow zsh.d
 
 # Adding GRUB themes
 sudo stow -t /usr grub
+
+##################################################################################################################################################################################
+##################################################################################################################################################################################
+#  ______          _       
+# |  ____|        | |      
+# | |__ ___  _ __ | |_ ___ 
+# |  __/ _ \| '_ \| __/ __|
+# | | | (_) | | | | |_\__ \
+# |_|  \___/|_| |_|\__|___/
+#                          
+
+# Define the fonts directory
+FONT_DIR="$HOME/.local/share/fonts"
+
+# Check if the directory exists, and create it if not
+if [ ! -d "$FONT_DIR" ]; then
+    mkdir -p "$FONT_DIR"
+    echo "Created directory: $FONT_DIR"
+else
+    echo "Directory already exists: $FONT_DIR"
+fi
+
+# Download the font zip file
+curl -L -o "$FONT_DIR/FantasqueSansMono.zip" "https://github.com/ryanoasis/nerd-fonts/releases/download/v3.2.1/FantasqueSansMono.zip"
+
+# Japanese Language Support
+curl -L -o "notosanscjk-jp.ttf" "https://github.com/googlefonts/noto-cjk/raw/main/Sans/Variable/TTF/NotoSansCJKjp-VF.ttf"
+
+# move the jp font to font_dir
+mv "notosanscjk-jp.ttf" "$FONT_DIR/notosanscjk-jp.ttf"
+
+# Unzip the downloaded font and refresh the font cache
+unzip "$FONT_DIR/FantasqueSansMono.zip" -d "$FONT_DIR"
+fc-cache -fv
+echo "Font installed successfully."
+
+# delete the zip file
+rm -rf "$FONT_DIR/FantasqueSansMono.zip"
 
 ##################################################################################################################################################################################
 ##################################################################################################################################################################################
