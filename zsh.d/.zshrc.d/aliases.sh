b1fdc1a5b01b8413685784c0db3e09a1eb8dda84
utkarshkrsingh <utkarshkrsingh@proton.me>
2024-12-22 19:56:20 +0530

    config --updated

--- zsh.d/.zshrc.d/aliases.sh
+++ zsh.d/.zshrc.d/aliases.sh
@@ -1,5 +1,6 @@
 # Alias
 alias update-grub="sudo grub-mkconfig -o /boot/grub/grub.cfg"
+alias update-system="paru -Syu --noconfirm"
 alias ls="exa -lahF --tree --level=1 --color=always --icons --sort=name --group-directories-first --no-time --no-user --no-filesize --git --no-permissions"
 alias v="nvim"
 alias bt="btop"
@@ -7,7 +8,6 @@ alias pipes="$HOME/.scripts/pipes.sh"
 alias pipesX="$HOME/.scripts/pipesX.sh"
 
 alias startpyenv="source ~/.venv/bin/activate"
-alias update-system="paru -Syu"
 alias gitlink="git remote set-url origin " ## gitlink https://<GITHUB-TOKEN>@github.com/nyxfr/<REPO-NAME>
 alias btop="btop --utf-force"
 
