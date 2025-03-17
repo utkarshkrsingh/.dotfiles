6cb9031811b6065fa78bd3cd918343d6187f16c4
utkarshkrsingh <utkarsh.isa.dev@gmail.com>
2025-03-18 02:55:51 +0530

    nvim-config --updated

--- nvim/.config/nvim/lua/utkarshkrsingh/plugins/lsp-config.lua
+++ nvim/.config/nvim/lua/utkarshkrsingh/plugins/lsp-config.lua
@@ -24,8 +24,8 @@ return {
 			end
 		end
 
-		vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
-		vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
+		vim.keymap.set("n", "fd", vim.lsp.buf.hover, {})
+		vim.keymap.set("n", "cd", vim.lsp.buf.definition, {})
 		vim.keymap.set("n", "ca", vim.lsp.buf.code_action, {})
 
 		local capabilities = require("cmp_nvim_lsp").default_capabilities()
