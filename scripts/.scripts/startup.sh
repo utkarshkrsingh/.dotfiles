64b17c438159bd908adf870d518a1fa5a674f6d5
utkarshkrsingh <utkarsh.isa.dev2gmail.com>
2025-05-02 15:30:35 +0530

    config --updated

---
+++ scripts/.scripts/startup.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Start Eww first
+eww daemon &
+sleep 1
+eww open bar0
+
+# Wait to ensure tray is initialized
+sleep 2
+
+# Start tray apps after system tray is ready
+copyq --start-server
+nm-applet --indicator
