15760fd979f424fbf4a72b91254bb76100d305c0
utkarshkrsingh <utkarsh.isa.dev@gmail.com>
2025-05-25 14:08:36 +0530

    config --updated

--- nvim/.config/nvim/lua/utkarshkrsingh/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/utkarshkrsingh/plugins/colorscheme.lua
@@ -3,6 +3,9 @@ return {
     lazy = false,
     priority = 1000,
     config = function()
+        require("catppuccin").setup({
+            transparent_background = true,
+        })
         vim.cmd("colorscheme catppuccin")
     end,
 }
