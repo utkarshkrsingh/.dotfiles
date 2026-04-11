8a5081a15067ffed3af435d938dfba1a172fcfef
Utkarsh Kumar Singh <utkarsh.isa.dev@gmail.com>
2026-04-12 03:49:55 +0530

    added yazi and toggleterm

---
+++ nvim/.config/nvim/lua/utkarshkrsingh/plugins/toggleterm.lua
@@ -0,0 +1,20 @@
+return {
+    {
+        "akinsho/toggleterm.nvim",
+        version = "*",
+        opts = {
+            size = 20,
+            open_mapping = [[<leader>t]], -- keybinding here
+            hide_numbers = true,
+            shade_terminals = true,
+            shading_factor = 2,
+            start_in_insert = true,
+            insert_mappings = true,
+            terminal_mappings = true,
+            persist_size = true,
+            direction = "horizontal", -- try "float" or "vertical"
+            close_on_exit = true,
+            shell = vim.o.shell,
+        },
+    },
+}
